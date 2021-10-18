60b13bbadedb29035bee29bf2ddba0adf7c6acd9
mltomlins0n <martin070891@gmail.com>
2021-10-18 17:55:00 +0100

    use state handling function on dropdown

--- S03-working-with-components/src/components/Expenses/ExpenseFilter.js
+++ S03-working-with-components/src/components/Expenses/ExpenseFilter.js
@@ -9,7 +9,7 @@ const ExpensesFilter = (props) => {
     <div className='expense-filter'>
       <div className='expense-filter__control'>
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
