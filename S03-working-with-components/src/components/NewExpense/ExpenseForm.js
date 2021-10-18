612b011ef112975822fc6ed5053813252a431e9a
mltomlins0n <martin070891@gmail.com>
2021-10-18 15:29:18 +0100

    Add Expense Form

--- S03-working-with-components/src/components/NewExpense/ExpenseForm.js
+++ S03-working-with-components/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,58 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
-  return <form></form>;
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
+
+  function amountChangeHandler(event) {
+    setEnteredAmount(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setEnteredDate(event.target.value);
+  }
+
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange={titleChangeHandler} />
+        </div>
+      </div>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
+        </div>
+      </div>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+  );
 }
 
 export default ExpenseForm;
