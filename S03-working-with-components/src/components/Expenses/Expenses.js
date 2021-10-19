4bab52303ff242e448f9f65b32bd0f0d95aa6ef8
mltomlins0n <martin070891@gmail.com>
2021-10-19 17:38:35 +0100

    Move content to ExpensesList

--- S03-working-with-components/src/components/Expenses/Expenses.js
+++ S03-working-with-components/src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import ExpenseFilter from './ExpenseFilter';
 
 function Expenses(props) {
@@ -15,29 +16,16 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
-    <div>
+    <li className='expenses-list'>
       <Card className='expenses'>
         <ExpenseFilter
           selected={filteredYear}
           onChangeFilter={filterChangehandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
-    </div>
+    </li>
   );
 }
 
