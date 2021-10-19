6d2915220afdda93e97a0b1101bfdca133dfb652
mltomlins0n <martin070891@gmail.com>
2021-10-19 18:28:33 +0100

    Convert expense amount to a number

--- S03-working-with-components/src/components/NewExpense/ExpenseForm.js
+++ S03-working-with-components/src/components/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ function ExpenseForm(props) {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount, // using '+' converts this to a number
       date: new Date(enteredDate),
     };
 
